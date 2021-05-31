bb493e1fd5f85cf935074dedd88a4e5616db8714
Natalie Enkin <natalie@enkin.co.il>
2021-05-31 10:45:43 +0300

    cleint- updating theme and using theme provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { AppStyled } from "./AppStyled";
+import { AppStyled, theme } from "./AppStyled";
+import { ThemeProvider } from "@material-ui/core/styles";
 import Home from "./pages/Home/Home";
+import SharedItem from "./pages/SharedItem/SharedItem";
 import Navbar from "./components/Navbar/Navbar";
 
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 function App() {
   return (
-    <AppStyled className="App">
-      <CssBaseline />
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </AppStyled>
+    <ThemeProvider theme={theme}>
+      <AppStyled className="App">
+        <CssBaseline />
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/items/:id">
+              <SharedItem />
+            </Route>
+          </Switch>
+        </Router>
+      </AppStyled>
+    </ThemeProvider>
   );
 }
 
--- client/src/AppStyled.js
+++ client/src/AppStyled.js
@@ -1,4 +1,17 @@
 import styled from "styled-components";
+import { createMuiTheme } from "@material-ui/core/styles";
+export const theme = createMuiTheme({
+  palette: {
+    primary1Color: "#1F4E79",
+    primary2Color: "#FFC000",
+    primary3Color: "#66E655",
+  },
+  typography: {
+    button: {
+      textTransform: "none",
+    },
+  },
+});
 
 export const AppStyled = styled.div`
   height: 100vh;
