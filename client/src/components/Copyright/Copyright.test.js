a77077d32cc5c0d7f185fff3ed5e4777114fe95b
Natalie Enkin <natalie@enkin.co.il>
2021-06-10 09:54:14 +0300

    client-improving test for copyright component

--- client/src/components/Copyright/Copyright.test.js
+++ client/src/components/Copyright/Copyright.test.js
@@ -1,8 +1,23 @@
 import { render } from "@testing-library/react";
+import { unmountComponentAtNode } from "react-dom";
 import Copyright from "./Copyright";
+let container = null;
 
-test("should have  ", () => {
-  const { queryByTitle } = render(<Copyright />);
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+test.skip("should have text with current year", () => {
+  const { queryByTitle } = render(<Copyright />, container);
   const text = queryByTitle("copyright");
   expect(text).toBeTruthy();
   expect(text.innerHTML).toContain("2021");
