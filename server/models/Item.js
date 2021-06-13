d3284cdf894008af3fc3d195127f4cf6c1dba079
Natalie Enkin <natalie@enkin.co.il>
2021-06-13 21:26:40 +0300

    creating an Item mongoose model

--- server/models/Item.js
+++ server/models/Item.js
@@ -1,2 +1,37 @@
-const Item = require("../items.json");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+    },
+    imgUrl: String,
+    // img: Buffer,
+    lastUsed: { type: Date },
+    publishedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    inUseBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+const Item = mongoose.model("Item", ItemSchema);
+
 module.exports = Item;
