3f6c3e3a5912bd25b6e5cb25cea553b679d1b96e
Natalie Enkin <natalie@enkin.co.il>
2021-06-13 22:46:36 +0300

    updating configuration with remote mongo server

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
 module.exports = {
-  mongoURI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@sharehub.btbi4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  mongoURI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@sharehub.btbi4.mongodb.net/Sharehub?retryWrites=true&w=majority`,
   jwtSecret: process.env.JWT_SECRET,
 };
